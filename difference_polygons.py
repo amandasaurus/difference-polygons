110d7cffd915951672438da5bc4cd46a0946795c
Rory McCann <rory@technomancy.org>
2016-01-02 16:57:13 +0100

    support many

--- difference_polygons.py
+++ difference_polygons.py
@@ -2,6 +2,7 @@ import sys, fiona
 import shapely.ops
 from shapely.geometry import shape, mapping
 from rtree import index
+import argparse
 
 def is_multi(geom):
     return geom.geom_type.startswith("Multi") or geom.geom_type == 'GeometryCollection'
@@ -34,7 +35,7 @@ def remove_zero_areas(geom):
 
     return result
 
-def do_difference(filename_a, filename_b, output_filename):
+def do_difference(filename_a, files_to_remove, output_filename, min_area):
     print "Reading in {}".format(filename_a)
     with fiona.open(filename_a) as fpa:
         output_crs = fpa.crs
@@ -42,11 +43,14 @@ def do_difference(filename_a, filename_b, output_filename):
         data_a = [shape(x['geometry']) for x in fpa]
 
     geom_index = index.Index()
-    print "Reading in {}".format(filename_b)
-    with fiona.open(filename_b) as fpb:
-        data_b = [shape(x['geometry']) for x in fpb]
-        for pos, b_obj in enumerate(data_b):
-            geom_index.insert(pos, b_obj.bounds)
+    polys_to_remove = []
+    for filename in files_to_remove:
+        print "Reading in {}".format(filename)
+        with fiona.open(filename) as fpb:
+            polys_to_remove.extend(shape(x['geometry']) for x in fpb)
+
+    for pos, b_obj in enumerate(polys_to_remove):
+        geom_index.insert(pos, b_obj.bounds)
 
 
     num_as = len(data_a)
@@ -54,17 +58,15 @@ def do_difference(filename_a, filename_b, output_filename):
     for idx, a_geom in enumerate(data_a):
         sys.stdout.write("Looking at object {:,} of {:,}\r".format(idx, num_as))
         sys.stdout.flush()
-        possible_bs = shapely.ops.cascaded_union([data_b[pos] for pos in geom_index.intersection(a_geom.bounds)])
+        possible_bs = shapely.ops.cascaded_union([polys_to_remove[pos] for pos in geom_index.intersection(a_geom.bounds)])
         new_geom = a_geom.difference(possible_bs)
         output_geoms.append(new_geom)
     print "\n"
 
     output_geoms = split_out_all_multis(output_geoms)
 
-    print "Removing empties"
-    print "There are {:,} geoms".format(len(output_geoms))
-    output_geoms = [x for x in output_geoms if x.area > 1e-8]
-    print "There are {:,} geoms".format(len(output_geoms))
+    if min_area:
+        output_geoms = [x for x in output_geoms if x.area >= min_area]
 
     print "Merging all geoms together..."
     output_geom = shapely.ops.cascaded_union(output_geoms)
@@ -79,8 +81,15 @@ def do_difference(filename_a, filename_b, output_filename):
             output_fp.write({'properties': {}, 'geometry': mapping(geom)})
 
 def main(argv):
-    filename_a, filename_b, output_filename = argv
-    do_difference(filename_a, filename_b, output_filename)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--land")
+    parser.add_argument("-r", "--remove", action="append")
+    parser.add_argument("-a", "--min-area", default=None, required=False, type=float)
+    parser.add_argument("-o", "--output")
+    parser.add_argument("-q", "--quiet", default=False, action="store_true")
+
+    args = parser.parse_args(argv)
+    do_difference(args.land, args.remove, args.output, args.min_area)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
