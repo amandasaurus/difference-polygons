34a213bc4de112086d1555db39df67a62a6b627c
Rory McCann <rory@technomancy.org>
2016-01-09 14:24:06 +0100

    new --queit option

--- difference_polygons/__init__.py
+++ difference_polygons/__init__.py
@@ -35,8 +35,9 @@ def remove_zero_areas(geom):
 
     return result
 
-def do_difference(filename_a, files_to_remove, output_filename, min_area):
-    print "Reading in {}".format(filename_a)
+def do_difference(filename_a, files_to_remove, output_filename, min_area, quiet=False):
+    if not quiet:
+        print "Reading in {}".format(filename_a)
     with fiona.open(filename_a) as fpa:
         output_crs = fpa.crs
 
@@ -45,7 +46,8 @@ def do_difference(filename_a, files_to_remove, output_filename, min_area):
     geom_index = index.Index()
     polys_to_remove = []
     for filename in files_to_remove:
-        print "Reading in {}".format(filename)
+        if not quiet:
+            print "Reading in {}".format(filename)
         with fiona.open(filename) as fpb:
             polys_to_remove.extend(shape(x['geometry']) for x in fpb)
 
@@ -56,26 +58,30 @@ def do_difference(filename_a, files_to_remove, output_filename, min_area):
     num_as = len(data_a)
     output_geoms = []
     for idx, a_geom in enumerate(data_a):
-        sys.stdout.write("Looking at object {:,} of {:,}\r".format(idx, num_as))
-        sys.stdout.flush()
+        if not quiet:
+            sys.stdout.write("Looking at object {:,} of {:,}\r".format(idx, num_as))
+            sys.stdout.flush()
         possible_bs = shapely.ops.cascaded_union([polys_to_remove[pos] for pos in geom_index.intersection(a_geom.bounds)])
         new_geom = a_geom.difference(possible_bs)
         output_geoms.append(new_geom)
-    print "\n"
+    if not quiet:
+        print "\n"
 
     output_geoms = split_out_all_multis(output_geoms)
 
     if min_area:
         output_geoms = [x for x in output_geoms if x.area >= min_area]
 
-    print "Merging all geoms together..."
+    if not quiet:
+        print "Merging all geoms together..."
     output_geom = shapely.ops.cascaded_union(output_geoms)
 
     # split into companent shapes
     output_geoms = split_out_all_multis(output_geom)
     
 
-    print "Writing output"
+    if not quiet:
+        print "Writing output"
     with fiona.open(output_filename, 'w', 'ESRI Shapefile', crs=output_crs, schema={'geometry': 'Polygon', 'properties': {}}) as output_fp:
         for geom in output_geoms:
             output_fp.write({'properties': {}, 'geometry': mapping(geom)})
@@ -87,10 +93,10 @@ def main(argv=None):
     parser.add_argument("-r", "--remove", action="append")
     parser.add_argument("-a", "--min-area", default=None, required=False, type=float)
     parser.add_argument("-o", "--output")
-    #parser.add_argument("-q", "--quiet", default=False, action="store_true")
+    parser.add_argument("-q", "--quiet", default=False, action="store_true")
 
     args = parser.parse_args(argv)
-    do_difference(args.land, args.remove, args.output, args.min_area)
+    do_difference(args.land, args.remove, args.output, args.min_area, args.quiet)
 
 if __name__ == '__main__':
     main()
